41ee28b3ab9db4f9e1e01ece163cea6ca910487f
Squirellex <kraim5133@gmail.com>
2020-10-07 21:21:53 +0300

    first commit

---
+++ src/app/item-manager/item-manager.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-manager',
+  templateUrl: './item-manager.component.html',
+  styleUrls: ['./item-manager.component.css']
+})
+export class ItemManagerComponent implements OnInit {
+  
+
+  
+
+  ngOnInit() {
+  }
+
+}
