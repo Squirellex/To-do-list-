d2e367c5287e2b1f6595ca48c161208b2abdcb72
Squirellex <kraim5133@gmail.com>
2020-10-08 22:03:43 +0300

    final commit

--- src/app/item-manager/item-manager.component.ts
+++ src/app/item-manager/item-manager.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+
 @Component({
   selector: 'app-item-manager',
   templateUrl: './item-manager.component.html',
   styleUrls: ['./item-manager.component.css']
 })
-export class ItemManagerComponent implements OnInit {
-  @Output() submit: EventEmitter<any> = new EventEmitter();
-  
-    item:string;
-    items=[];
 
+export class ItemManagerComponent implements OnInit {
+    item:string;
+    items = []
+  
 
     ngOnInit() {
     }
@@ -18,7 +19,5 @@ export class ItemManagerComponent implements OnInit {
     onClick(){
       this.items.push({name: this.item});
       this.item = '';
-      this.submit.emit(this.items);
     }
-
 }
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -3,8 +3,8 @@ import {Component, Input} from '@angular/core';
 @Component({
     selector: 'app-item',
     templateUrl: './item.component.html',
-    styleUrls: ['./item.component.css']
+    styleUrls: ['./item.component.css'],
 })
 export class ItemComponent {
-    @Input() Title = submit
+    @Input() todoTitle;
 }
